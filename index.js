28836b352675cbfa1fc1bc68d8e54b73e97ceb84
Doug Ty <git@dougty.com>
2016-06-09 14:23:35 -0600

    Custom restart script

---
+++ index.js
@@ -0,0 +1,46 @@
+var respawn = require( 'respawn' );
+
+var bot = respawn( ['node', 'bot.js'],
+	{
+		maxRestarts: -1,
+		sleep: 1000
+	});
+
+var log = [];
+var logSize = 16;
+function logData( data )
+{
+	data = data.toString().split( '\n' );
+	for ( var i in data )
+		log.push( data[i] );
+	
+	while ( log.length > logSize )
+		log.shift();
+}
+
+bot.on( 'spawn', function()
+	{
+		console.log( 'spawning bot process...' );
+	});
+
+bot.on( 'stdout', function( data )
+	{
+		process.stdout.write( data );
+		logData( data );
+	});
+
+bot.on( 'stderr', function( data )
+	{
+		process.stdout.write( data );
+		logData( data );
+	});
+
+bot.on( 'exit', function( code )
+	{
+		console.log( 'process exited with code ' + code + ', restarting...' );
+		if ( code != 0 )
+			require('fs').writeFileSync( './crash.log', log.join(''), 'utf8' );
+		log = [];
+	});
+
+bot.start();
