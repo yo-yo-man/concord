f127e8c95713c8f726bb730001b290ec3baab771
Doug Ty <git@dougty.com>
2016-06-09 14:19:41 -0600

    Renamed index.js to bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var crashlog = './crash.log';
-
 var Discordie = require( 'discordie' );
 var fs = require( 'fs' );
 
@@ -19,25 +17,20 @@ client.Dispatcher.on( 'GATEWAY_READY', e =>
 		require('./plugins.js').load( client );
 		console.log( 'bot is ready!' );
 		
-		if ( fs.existsSync( crashlog ) )
+		if ( fs.existsSync( './crash.log' ) )
 		{
-			var log = require('fs').readFileSync( crashlog, 'utf8' );
+			var log = fs.readFileSync( './crash.log', 'utf8' );
 			var owner = client.Users.get( settings.get( 'config', 'owner_id' ) );
 			if ( owner )
 				owner.openDM().then( d => d.sendMessage( _.fmt( '```\n%s\n```', log ) ) );
-			fs.unlinkSync( crashlog );
+			else
+				console.log( 'WARNING: no owner to send crash log to' );
+			fs.unlinkSync( './crash.log' );
 		}
 	});
 
 client.Dispatcher.onAny( ( type, e ) =>
 	{
-		if ( [ 'GATEWAY_RESUMED', 'DISCONNECTED', 'GUILD_UNAVAILABLE', 'GUILD_CREATE', 'GUILD_DELETE', 'CHANNEL_DELETE' ].indexOf( type ) != -1 )
-			return console.log('<' + type + '> ' + (e.error || '') );
-	});
-
-process.on('uncaughtException', err =>
-	{
-		fs.writeFileSync( crashlog, err.stack, 'utf8' );
-		console.error( err.stack );
-		process.exit( 1 );
+		if ( [ 'GATEWAY_RESUMED', 'DISCONNECTED', 'GUILD_UNAVAILABLE', 'GUILD_CREATE', 'GUILD_DELETE' ].indexOf( type ) != -1 )
+			return console.log('<' + type + '> ' + (e.error || e.guildId || e.guild.id || '') );
 	});
