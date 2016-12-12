341448880d40fe5620bcf5f372dad614beae9f99
Doug Ty <git@dougty.com>
2016-12-12 07:24:56 -0700

    Added clearafter command

--- plugins/moderation.js
+++ plugins/moderation.js
@@ -5,19 +5,62 @@ var permissions = require( '../permissions.js' );
 var settings = require( '../settings.js' );
 var _ = require( '../helper.js' );
 
+function clearMessages( msg, limit, target, after )
+{	
+	if ( !client.User.can( permissions.discord.Text.MANAGE_MESSAGES, msg.channel ) )
+		return msg.channel.sendMessage( "invalid 'manage messages' permission in this channel" );
+	
+	if ( target )
+	{
+		target = commands.findTarget( msg, target );
+		if ( target === false )
+			return;
+	}
+	else
+		limit++; // clear the user's !clear command as well
+	
+	var hardLimit = 100; // max API will allow
+	var lookback = hardLimit; // number of messages to look back into
+	msg.channel.fetchMessages( lookback, msg, after ).then( () =>
+		{
+			var toDelete = [];
+			for ( var i = msg.channel.messages.length-1; i >= 0; i-- )
+			{
+				var message = msg.channel.messages[i];
+				
+				if ( message.deleted || ( target !== false && target.id != message.author.id ) )
+					continue;
+				
+				if ( toDelete.length >= hardLimit )
+					break;
+					
+				toDelete.push( message );
+			}
+			
+			client.Messages.deleteMessages( toDelete ).then( () =>
+				{
+					var byUser = '';
+					if ( target !== false )
+						byUser = _.fmt( ' by `%s`', _.nick( target ) );
+					var numCleared = toDelete.length-1; // subtract user's !clear command
+					msg.channel.sendMessage( _.fmt( '`%s` cleared `%s` messages%s', _.nick( msg.member ), numCleared, byUser ) );
+				}).catch( e => msg.channel.sendMessage( _.fmt( 'error deleting messages: `%s`', e.message ) ) );
+		}).catch( e => msg.channel.sendMessage( _.fmt( 'error fetching messages: `%s`', e.message ) ) );
+}
+
 commands.register( {
 	category: 'moderation',
 	aliases: [ 'clear' ],
 	help: 'clear messages',
 	flags: [ 'admin_only', 'no_pm' ],
-	args: '[limit=100] [user]',
+	args: 'limit=100 [user]',
 	callback: ( client, msg, args ) =>
 	{
 		if ( !args )
 			return msg.channel.sendMessage( '`' + commands.generateHelp( commands.getCMD( 'clear' ) ) + '`' );
 		
 		var split = args.split( ' ' );
-		var limit = split[0] || 100;
+		var limit = split[0] || 99;
 		var target = split[1] || false;
 		
 		if ( isNaN( limit ) )
@@ -26,44 +69,28 @@ commands.register( {
 		if ( parseInt( limit ) > 100 )
 			return msg.channel.sendMessage( _.fmt( 'can only delete `100` messages at a time' ) );
 		
-		if ( !client.User.can( permissions.discord.Text.MANAGE_MESSAGES, msg.channel ) )
-			return msg.channel.sendMessage( "invalid 'manage messages' permission in this channel" );
+		clearMessages( msg, limit, target, null );
+	}});
+
+commands.register( {
+	category: 'moderation',
+	aliases: [ 'clearafter' ],
+	help: 'clear messages after a message ID',
+	flags: [ 'admin_only', 'no_pm' ],
+	args: 'messageID [user]',
+	callback: ( client, msg, args ) =>
+	{
+		if ( !args )
+			return msg.channel.sendMessage( '`' + commands.generateHelp( commands.getCMD( 'clear' ) ) + '`' );
 		
-		if ( target )
-		{
-			target = commands.findTarget( msg, target );
-			if ( target === false )
-				return;
-		}
-		else
-			limit++; // clear the user's !clear command as well
+		var split = args.split( ' ' );
+		var after = split[0];
+		var target = split[1] || false;
 		
-		var lookback = 100; // number of messages to look back into
-		msg.channel.fetchMessages( lookback, msg ).then( () =>
-			{
-				var toDelete = [];
-				for ( var i = msg.channel.messages.length-1; i >= 0; i-- )
-				{
-					var message = msg.channel.messages[i];
-					
-					if ( message.deleted || ( target !== false && target.id != message.author.id ) )
-						continue;
-					
-					if ( toDelete.length >= limit )
-						break;
-						
-					toDelete.push( message );
-				}
-				
-				client.Messages.deleteMessages( toDelete ).then( () =>
-					{
-						var byUser = '';
-						if ( target !== false )
-							byUser = _.fmt( ' by `%s`', _.nick( target ) );
-						var numCleared = toDelete.length-1; // subtract user's !clear command
-						msg.channel.sendMessage( _.fmt( '`%s` cleared `%s` messages%s', _.nick( msg.member ), numCleared, byUser ) );
-					});
-			});
+		if ( isNaN( after ) )
+			return msg.channel.sendMessage( _.fmt( '`%s` is not a numeric message ID', after ) );
+		
+		clearMessages( msg, 99, target, after );
 	}});
 
 var client = null;
