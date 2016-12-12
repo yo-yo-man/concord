94eb9024d93277dcaa2eef23da4f9278de3c0f0f
Doug Ty <git@dougty.com>
2016-12-12 07:54:40 -0700

    Fixed clearafter command

--- plugins/moderation.js
+++ plugins/moderation.js
@@ -7,6 +7,12 @@ var _ = require( '../helper.js' );
 
 function clearMessages( msg, limit, target, after )
 {	
+	if ( isNaN( limit ) )
+		return msg.channel.sendMessage( _.fmt( '`%s` is not a number', limit ) );
+	
+	if ( parseInt( limit ) > 100 )
+		return msg.channel.sendMessage( _.fmt( 'can only delete `100` messages at a time' ) );
+	
 	if ( !client.User.can( permissions.discord.Text.MANAGE_MESSAGES, msg.channel ) )
 		return msg.channel.sendMessage( "invalid 'manage messages' permission in this channel" );
 	
@@ -16,22 +22,28 @@ function clearMessages( msg, limit, target, after )
 		if ( target === false )
 			return;
 	}
-	else
+	else if ( !after )
 		limit++; // clear the user's !clear command as well
 	
-	var hardLimit = 100; // max API will allow
-	var lookback = hardLimit; // number of messages to look back into
-	msg.channel.fetchMessages( lookback, msg, after ).then( () =>
+	var before = msg;
+	if ( after ) before = null;
+	
+	limit = Math.min( limit, 100 );
+	var lookback = 100; // number of messages to look back into
+	msg.channel.fetchMessages( lookback, null, after ).then( () =>
 		{
+			var msglist = msg.channel.messages;
+			if ( after ) msglist.reverse();
+			
 			var toDelete = [];
-			for ( var i = msg.channel.messages.length-1; i >= 0; i-- )
+			for ( var i = msglist.length-1; i >= 0; i-- )
 			{
-				var message = msg.channel.messages[i];
+				var message = msglist[i];
 				
 				if ( message.deleted || ( target !== false && target.id != message.author.id ) )
 					continue;
 				
-				if ( toDelete.length >= hardLimit )
+				if ( toDelete.length >= limit )
 					break;
 					
 				toDelete.push( message );
@@ -42,7 +54,8 @@ function clearMessages( msg, limit, target, after )
 					var byUser = '';
 					if ( target !== false )
 						byUser = _.fmt( ' by `%s`', _.nick( target ) );
-					var numCleared = toDelete.length-1; // subtract user's !clear command
+					var numCleared = toDelete.length;
+					if ( !after ) numCleared -= 1;  // subtract user's !clear command
 					msg.channel.sendMessage( _.fmt( '`%s` cleared `%s` messages%s', _.nick( msg.member ), numCleared, byUser ) );
 				}).catch( e => msg.channel.sendMessage( _.fmt( 'error deleting messages: `%s`', e.message ) ) );
 		}).catch( e => msg.channel.sendMessage( _.fmt( 'error fetching messages: `%s`', e.message ) ) );
@@ -56,18 +69,9 @@ commands.register( {
 	args: 'limit=100 [user]',
 	callback: ( client, msg, args ) =>
 	{
-		if ( !args )
-			return msg.channel.sendMessage( '`' + commands.generateHelp( commands.getCMD( 'clear' ) ) + '`' );
-		
 		var split = args.split( ' ' );
 		var limit = split[0] || 99;
 		var target = split[1] || false;
-		
-		if ( isNaN( limit ) )
-			return msg.channel.sendMessage( _.fmt( '`%s` is not a number', limit ) );
-		
-		if ( parseInt( limit ) > 100 )
-			return msg.channel.sendMessage( _.fmt( 'can only delete `100` messages at a time' ) );
 		
 		clearMessages( msg, limit, target, null );
 	}});
@@ -77,20 +81,17 @@ commands.register( {
 	aliases: [ 'clearafter' ],
 	help: 'clear messages after a message ID',
 	flags: [ 'admin_only', 'no_pm' ],
-	args: 'messageID [user]',
+	args: 'messageID [limit=100]',
 	callback: ( client, msg, args ) =>
-	{
-		if ( !args )
-			return msg.channel.sendMessage( '`' + commands.generateHelp( commands.getCMD( 'clear' ) ) + '`' );
-		
+	{	
 		var split = args.split( ' ' );
 		var after = split[0];
-		var target = split[1] || false;
+		var limit = split[1] || 99;
 		
 		if ( isNaN( after ) )
 			return msg.channel.sendMessage( _.fmt( '`%s` is not a numeric message ID', after ) );
 		
-		clearMessages( msg, 99, target, after );
+		clearMessages( msg, limit, false, after );
 	}});
 
 var client = null;
